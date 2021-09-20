59b14d5c12f509fc31ed1b3942b77f9645746f39
DESKTOP-4J11F7B\Adiministrator <golden.test2021@mail.ru>
2021-09-20 07:50:28 +0300

    -main creating frontend by react

---
+++ src/containers/programing/class_generator.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import {TextField, Container, Grid} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
+
+import Nav from '../layout/nav'
+
+const useStyles = makeStyles((theme) => ({
+  codeLine: {
+    fontSize: 20,
+    paddingTop: 5
+  },
+}))
+function ClassGenerator() {
+  const classes = useStyles();
+  const [className, setClassName] = useState('')
+
+  return (
+    <div>
+      <Nav />
+      <Container maxWidth='lg'>
+        <Grid
+          container
+          direction='row'
+          justify='flex-start'
+          alignItems='center'
+          style={{marginTop: 100, marginBottom: 10}}
+        >
+          <TextField
+            id='outlined-multiline-static'
+            label='Class name'
+            value={className}
+            onChange={(e) => setClassName(e.target.value)}
+            variant='outlined'
+            autoComplete='off'
+          />
+        </Grid>
+        { className &&
+          (
+            <div style={{paddingTop: 50}}>
+              <div className={classes.codeLine}>class {className}():</div>
+              <div className={classes.codeLine}>&emsp;def __init__(self):</div>
+              <div className={classes.codeLine}>&emsp;&emsp;super().__init__()</div>
+              <div className={classes.codeLine}>&emsp;def run(self):</div>
+              <div className={classes.codeLine}>&emsp;&emsp;pass</div>
+              <div className={classes.codeLine}>if __name__ == '__main__':</div>
+              <div className={classes.codeLine}>&emsp;{className}.run()</div>
+            </div>
+          )
+        }
+        
+      </Container>
+    </div>
+  )
+}
+export default ClassGenerator
---
+++ src/router.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './containers/programing/home'
+import List from './containers/programing/list'
+import Json from './containers/programing/json'
+import Base64 from './containers/programing/base64'
+import Regard from './containers/programing/regard'
+import Cron from './containers/programing/cron'
+import Binary from './containers/programing/binary'
+import TextComparison from './containers/programing/text_comparison'
+import ClassGenerator from './containers/programing/class_generator'
+import SimpleSavingsCalculator from './containers/financial/simple_savings_calculator'
+import SavingsCalculator from './containers/financial/savings_calculator'
+import Calculator from './containers/financial/calculator'
+import LoanCalculator from './containers/financial/loan_calculator'
+
+export default function Routes() {
+    return (
+      <Router>
+        <Route exact path='/' component={Home}>
+        </Route>
+        <Route path='/list' component={List}>
+        </Route>
+        <Route path='/json' component={Json}>
+        </Route>
+        <Route path='/base64' component={Base64}>
+        </Route>
+        <Route path='/regard' component={Regard}>
+        </Route>
+        <Route path='/cron' component={Cron}>
+        </Route>
+        <Route path='/binary' component={Binary}>
+        </Route>
+        <Route path='/textComparison' component={TextComparison}>
+        </Route>
+        <Route path='/classGenerator' component={ClassGenerator}>
+        </Route>
+        <Route path='/simpleSavingCalculator' component={SimpleSavingsCalculator}>
+        </Route>
+        <Route path='/savingCalculator' component={SavingsCalculator}>
+        </Route>
+        <Route path='/calculator' component={Calculator}>
+        </Route>
+        <Route path='/loanCalculator' component={LoanCalculator}>
+        </Route>
+      </Router>
+    );
+}
