ae06325d4980f943c9fbcf3cdd8dc231424eaaaf
DevStar234 <90933603+DevStar234@users.noreply.github.com>
2021-10-25 09:26:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom'
 ReactDOM.render(( 
       <App />
     ), document.getElementById('root')
-);
+); 
           
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
