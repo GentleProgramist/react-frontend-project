dbdb14b492041adc5e58db4f4626e23bc0013774
DevStar234 <90933603+DevStar234@users.noreply.github.com>
2021-10-25 09:26:38 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(( 
-      <App />
+      <App /> 
     ), document.getElementById('root')
 ); 
          
