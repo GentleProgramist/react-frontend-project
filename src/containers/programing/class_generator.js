8d319b6955a9d304983272cc0fa8c15fa2392b15
DevStar234 <90933603+DevStar234@users.noreply.github.com>
2021-11-15 12:36:27 +0300

    Update class_generator.js

--- src/containers/programing/class_generator.js
+++ src/containers/programing/class_generator.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {TextField, Container, Grid} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles';
-
+ 
 import Nav from '../layout/nav'
 
 const useStyles = makeStyles((theme) => ({
